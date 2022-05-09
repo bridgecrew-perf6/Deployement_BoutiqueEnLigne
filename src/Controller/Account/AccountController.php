c1916f90b2836317bed602b8ae6e651ca9fcc58d
faridgi <89008266+faridgi@users.noreply.github.com>
2022-05-09 09:19:08 +0200

    ajout edit Profile& Pass

--- src/Controller/Account/AccountController.php
+++ src/Controller/Account/AccountController.php
@@ -3,11 +3,18 @@
 namespace App\Controller\Account;
 
 use App\Entity\Order;
+use App\Entity\User;
+use App\Form\EditProfileType;
+use App\Form\ResetPasswordRequestFormType;
+use App\Repository\AddressRepository;
 use App\Repository\OrderRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-
 
 /**
  * @Route("/account")
@@ -17,13 +24,69 @@ class AccountController extends AbstractController
     /**
      * @Route("/", name="account")
      */
-    public function index(OrderRepository $repoOrder): Response
+    public function index(OrderRepository $repoOrder, AddressRepository $addressRepository): Response
     {
         $orders = $repoOrder->findBy(['isPaid' => true, 'user' => $this->getUser()], ['id' => 'DESC']);
-
+        
+        // $repoUser = $this->getUser()->getAddresses()->getValues();
+        // dd($repoUser);
+    
         return $this->render('account/index.html.twig',[
             'orders' => $orders,
+            'addresses' => $addressRepository->findBy(['user' => $this->getUser()])
         ]);
+    }
+
+
+    /**
+     * @Route("/profile/edit", name="account_profile_edit")
+     */
+    public function editProfile(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        
+        $user = $this->getUser();
+        $form = $this->createForm(EditProfileType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $this->getUser();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('account_message', 'Votre profil a été modifié avec succès');
+            return $this->redirectToRoute('account');
+        }
+
+ 
+
+        return $this->renderForm('account/editProfile.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    
+    /**
+     * @Route("/pass/edit", name="account_pass_edit")
+     */
+    public function editPass(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
+    
+        if($request->isMethod('POST')){
+            $user = $this->getUser();
+            //on vérifie si les deux mots de passe sont identiques
+            if($request->request->get('pass') == $request->request->get('pass2')){
+                $user->setPassword($userPasswordHasher->hashPassword($user, $request->get('pass')));
+                $entityManager->persist($user);
+                $entityManager->flush();
+                $this->addFlash('message', 'Mot de passe mis à jour avec succès');
+
+                return $this->redirectToRoute('account');
+            }else{
+                $this->addFlash('error', 'Les deux mots de passe ne sont pas identiques');
+            }
+        }
+
+        return $this->renderForm('account/editPass.html.twig');
     }
 
     /**
